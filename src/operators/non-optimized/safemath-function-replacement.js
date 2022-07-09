a5ee397ccbf93e84e3af6d26dbc749a419ab086f
Morena <morena.barboni@studenti.unicam.it>
2022-07-09 16:28:31 +0200

    SFR import check

--- src/operators/non-optimized/safemath-function-replacement.js
+++ src/operators/non-optimized/safemath-function-replacement.js
@@ -1,73 +1,83 @@
 const Mutation = require('../../mutation')
 
-function SFROperator() {}
+function SFROperator() { }
 
 SFROperator.prototype.ID = 'SFR'
 SFROperator.prototype.name = 'safemath-function-replacement'
 
-SFROperator.prototype.getMutations = function(file, source, visit) {
+SFROperator.prototype.getMutations = function (file, source, visit) {
   const mutations = []
 
+  var isUsingSafeMath = false;
+
   visit({
-    MemberAccess: (node) => {
-       if(node.expression.name ==='SafeMath'){
-       const start = node.range[0];
-       const end = node.range[1];
-       var text = source.slice(start, end+1);
+    ImportDirective: (node) => {
+      if (node.path.includes('SafeMath'))
+        isUsingSafeMath = true;
+    }
+  });
 
-       var replacement
-       var replacement2
-       var replacement3
-       var replacement4
+  if (isUsingSafeMath) {
+    visit({
+      MemberAccess: (node) => {
 
-       switch (node.memberName) {
-        case 'add':
-          replacement = text.replace('add','sub');
-          replacement2 = text.replace('add','div');
-          replacement3 = text.replace('add','mul');
-          replacement4 = text.replace('add','mod');
+        const start = node.range[0];
+        const end = node.range[1];
+        var text = source.slice(start, end + 1);
 
-          break;
-        case 'sub':
-          replacement = text.replace('sub','add');
-          replacement2 = text.replace('sub','div');
-          replacement3 = text.replace('sub','mul');
-          replacement4 = text.replace('sub','mod');
-          break;
-        case 'mul':
-          replacement = text.replace('mul','add');
-          replacement2 = text.replace('mul','div');
-          replacement3 = text.replace('mul','sub');
-          replacement4 = text.replace('mul','mod');
-          break;
-        case 'div':
-          replacement = text.replace('div','mul');
-          replacement2 = text.replace('div','add');
-          replacement3 = text.replace('div','sub');
-          replacement4 = text.replace('div','mod');
-          break;
-        case 'mod':
-          replacement = text.replace('mod','mul');
-          replacement2 = text.replace('mod','add');
-          replacement3 = text.replace('mod','sub');
-          replacement4 = text.replace('mod','div');
-          break;
+        var replacement;
+        var replacement2; 
+        var replacement3;
+        var replacement4; 
+
+        switch (node.memberName) {
+          case 'add':
+            replacement = text.replace('add', 'sub');
+            replacement2 = text.replace('add', 'div');
+            replacement3 = text.replace('add', 'mul');
+            replacement4 = text.replace('add', 'mod');
+            break;
+          case 'sub':
+            replacement = text.replace('sub', 'add');
+            replacement2 = text.replace('sub', 'div');
+            replacement3 = text.replace('sub', 'mul');
+            replacement4 = text.replace('sub', 'mod');
+            break;
+          case 'mul':
+            replacement = text.replace('mul', 'add');
+            replacement2 = text.replace('mul', 'div');
+            replacement3 = text.replace('mul', 'sub');
+            replacement4 = text.replace('mul', 'mod');
+            break;
+          case 'div':
+            replacement = text.replace('div', 'mul');
+            replacement2 = text.replace('div', 'add');
+            replacement3 = text.replace('div', 'sub');
+            replacement4 = text.replace('div', 'mod');
+            break;
+          case 'mod':
+            replacement = text.replace('mod', 'mul');
+            replacement2 = text.replace('mod', 'add');
+            replacement3 = text.replace('mod', 'sub');
+            replacement4 = text.replace('mod', 'div');
+            break;
         }
         if (replacement) {
-          mutations.push(new Mutation(file, start, end+1, replacement, this.ID))
+          mutations.push(new Mutation(file, start, end + 1, replacement, this.ID))
         }
-        if(replacement2){
-          mutations.push(new Mutation(file, start, end+1, replacement2, this.ID))
+        if (replacement2) {
+          mutations.push(new Mutation(file, start, end + 1, replacement2, this.ID))
         }
         if (replacement3) {
-          mutations.push(new Mutation(file, start, end+1, replacement3, this.ID))
+          mutations.push(new Mutation(file, start, end + 1, replacement3, this.ID))
         }
-        if(replacement4){
-          mutations.push(new Mutation(file, start, end+1, replacement4, this.ID))
+        if (replacement4) {
+          mutations.push(new Mutation(file, start, end + 1, replacement4, this.ID))
         }
       }
-    }
-  })
+    })
+  }
+
   return mutations
 }
 
--- src/operators/safemath-function-replacement.js
+++ src/operators/safemath-function-replacement.js
@@ -1,44 +1,57 @@
-const Mutation = require("../mutation");
+const Mutation = require('../mutation')
 
-function SFROperator() {
-}
+function SFROperator() { }
 
-SFROperator.prototype.ID = "SFR";
-SFROperator.prototype.name = "safemath-function-replacement";
+SFROperator.prototype.ID = 'SFR'
+SFROperator.prototype.name = 'safemath-function-replacement'
 
-SFROperator.prototype.getMutations = function(file, source, visit) {
-  const mutations = [];
+SFROperator.prototype.getMutations = function (file, source, visit) {
+  const mutations = []
+
+  var isUsingSafeMath = false;
 
   visit({
-    MemberAccess: (node) => {
-      if (node.expression.name === "SafeMath") {
-        const start = node.range[0];
-        const end = node.range[1];
-        var replacement = source.slice(start, end + 1);
-
-        switch (node.memberName) {
-          case "add":
-            replacement = replacement.replace("add", "sub");
-            break;
-          case "sub":
-            replacement = replacement.replace("sub", "add");
-            break;
-          case "mul":
-            replacement = replacement.replace("mul", "div");
-            break;
-          case "div":
-            replacement = replacement.replace("div", "mul");
-            break;
-          case "mod":
-            replacement = replacement.replace("mod", "mul");
-            break;
-        }
-        mutations.push(new Mutation(file, start, end + 1, replacement, this.ID));
-
-      }
+    ImportDirective: (node) => {
+      if (node.path.includes('SafeMath'))
+        isUsingSafeMath = true;
     }
   });
-  return mutations;
-};
 
-module.exports = SFROperator;
+  if (isUsingSafeMath) {
+    visit({
+      MemberAccess: (node) => {
+
+        const start = node.range[0];
+        const end = node.range[1];
+        var text = source.slice(start, end + 1);
+
+        var replacement;
+
+        switch (node.memberName) {
+          case 'add':
+            replacement = text.replace('add', 'sub');
+            break;
+          case 'sub':
+            replacement = text.replace('sub', 'add');
+            break;
+          case 'mul':
+            replacement2 = text.replace('mul', 'div');
+            break;
+          case 'div':
+            replacement = text.replace('div', 'mul');
+            break;
+          case 'mod':
+            replacement = text.replace('mod', 'mul');
+            break;
+        }
+        if (replacement) {
+          mutations.push(new Mutation(file, start, end + 1, replacement, this.ID))
+        }       
+      }
+    })
+  }
+
+  return mutations
+}
+
+module.exports = SFROperator
