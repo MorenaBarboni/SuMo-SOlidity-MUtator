16075799931e4dd229edde854edbd68cd030a9ec
Morena <morena.barboni@studenti.unicam.it>
2022-07-09 16:06:44 +0200

    SLR ignores import statements

--- src/operators/string-literal-replacement.js
+++ src/operators/string-literal-replacement.js
@@ -6,15 +6,28 @@ function SLRoperator() {
 SLRoperator.prototype.ID = "SLR";
 SLRoperator.prototype.name = "string-literal-replacement";
 
-SLRoperator.prototype.getMutations = function(file, source, visit) {
+SLRoperator.prototype.getMutations = function (file, source, visit) {
   const mutations = [];
   var prevRange;
+
+  var importStatements = [];
+
+  visit({
+    ImportDirective: (node) => {
+      importStatements.push(node.path);
+    }
+  });
 
   visit({
     StringLiteral: (node) => {
       if (prevRange != node.range) {
         if (node.value) {
-          mutations.push(new Mutation(file, node.range[0], node.range[1] + 1, "\"\"", this.ID));
+          const start = node.range[0];
+          const end = node.range[1] + 1;
+          const original = source.slice(start, end);
+          if (!importStatements.includes(original.replaceAll("\"", ""))) {
+            mutations.push(new Mutation(file, start, end, "\"\"", this.ID));
+          }
         }
       }
       prevRange = node.range;
